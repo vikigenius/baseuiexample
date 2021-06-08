a8acecda312ed54b9a055e95ab834d959a138aac
vikigenius <v9balasu@uwaterloo.ca>
2021-06-08 17:56:26 -0400

    Updated example: reproduced dark theme error

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Client as Styletron} from 'styletron-engine-atomic';
+import {Provider as StyletronProvider} from 'styletron-react';
+import { BaseProvider, DarkTheme } from 'baseui';
+import {
+  ALIGN,
+  HeaderNavigation,
+  StyledNavigationItem as NavigationItem,
+  StyledNavigationList as NavigationList,
+} from 'baseui/header-navigation';
+import React from 'react';
 
-interface AppProps {}
+const engine = new Styletron();
 
-function App({}: AppProps) {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
+
+function App(): React.ReactElement {
   // Return the App component.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={DarkTheme}>
+        <HeaderNavigation>
+          <NavigationList $align={ALIGN.left}>
+            <NavigationItem>DarkTheme</NavigationItem>
+          </NavigationList>
+        </HeaderNavigation>
+      </BaseProvider>
+    </StyletronProvider>
   );
 }
 
